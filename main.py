14cc8d2389810fddf2510c540c829e0908c59047
unknown <fatimaqadeer26@gmail.com>
2026-07-24 15:42:27 +0500

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, Response, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 
 app = FastAPI()
 
 
-# Request model
+# Request Models
 class TaskCreate(BaseModel):
     title: str
+
+
+class TaskUpdate(BaseModel):
+    title: str
+    done: bool
 
 
 # In-memory list of tasks
@@ -18,6 +23,7 @@ tasks = [
 ]
 
 
+# Root endpoint
 @app.get("/")
 async def root():
     return {
@@ -27,16 +33,21 @@ async def root():
     }
 
 
+# Health endpoint
 @app.get("/health")
 async def health():
-    return {"status": "ok"}
+    return {
+        "status": "ok"
+    }
 
 
+# Get all tasks
 @app.get("/tasks")
 async def get_tasks():
     return tasks
 
 
+# Get a single task
 @app.get("/tasks/{id}")
 async def get_task(id: int):
     for task in tasks:
@@ -49,6 +60,7 @@ async def get_task(id: int):
     )
 
 
+# Create a new task
 @app.post("/tasks", status_code=status.HTTP_201_CREATED)
 async def create_task(task: TaskCreate):
 
@@ -67,3 +79,40 @@ async def create_task(task: TaskCreate):
     tasks.append(new_task)
 
     return new_task
+
+
+# Update a task
+@app.put("/tasks/{id}")
+async def update_task(id: int, updated_task: TaskUpdate):
+
+    if not updated_task.title.strip():
+        return JSONResponse(
+            status_code=400,
+            content={"error": "Title cannot be empty"}
+        )
+
+    for task in tasks:
+        if task["id"] == id:
+            task["title"] = updated_task.title
+            task["done"] = updated_task.done
+            return task
+
+    return JSONResponse(
+        status_code=404,
+        content={"error": f"Task {id} not found"}
+    )
+
+
+# Delete a task
+@app.delete("/tasks/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_task(id: int):
+
+    for task in tasks:
+        if task["id"] == id:
+            tasks.remove(task)
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    return JSONResponse(
+        status_code=404,
+        content={"error": f"Task {id} not found"}
+    )
